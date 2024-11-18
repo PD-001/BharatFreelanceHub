78d593576b828085da4830177eb20814ad86952f
PD-001 <110840658+PD-001@users.noreply.github.com>
2024-11-18 11:10:20 +0530

    Update client_homepage_firebase.js

--- client/client_homepage_firebase.js
+++ client/client_homepage_firebase.js
@@ -4,12 +4,7 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyChL4PXmR5RHmNnlVc6PiocbEsq3ygpD3E",
-  authDomain: "bharatfreelance-hub-538c2.firebaseapp.com",
-  projectId: "bharatfreelance-hub-538c2",
-  storageBucket: "bharatfreelance-hub-538c2.appspot.com",
-  messagingSenderId: "297970426246",
-  appId: "1:297970426246:web:fc266f818611f1c14a8c07"
+ 
 };
 
 // Initialize Firebase
